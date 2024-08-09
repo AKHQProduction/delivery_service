82cdfa22a5881cfc49ef1516aa8dcd392df0ff89
Andrii <andryxakulishow@gmail.com>
2024-08-09 17:49:04 +0300

    Pull

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import GeoMapPage from "./pages/geoMapPage";
+import "./App.css";
 
 function App() {
   return (
--- frontend/src/components/mapComponent.tsx
+++ frontend/src/components/mapComponent.tsx
@@ -1,44 +1,50 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-import 'leaflet-routing-machine';
-import { generateRoute } from '../api/generateRoute';
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet-routing-machine";
+import { generateRoute } from "../api/generateRoute";
 
 export const MapComponent: React.FC = () => {
   const mapRef = useRef<HTMLDivElement>(null);
   const mapInstance = useRef<L.Map | null>(null);
-  const [routeControl, setRouteControl] = useState<L.Routing.Control | null>(null);
+  const [routeControl, setRouteControl] = useState<L.Routing.Control | null>(
+    null
+  );
 
   useEffect(() => {
     if (mapRef.current && !mapInstance.current) {
-      mapInstance.current = L.map(mapRef.current).setView([51.505, -0.09], 13);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors',
-      }).addTo(mapInstance.current);
+      mapInstance.current = L.map(mapRef.current, {
+        center: [49.444433, 32.059767],
+        zoom: 13,
+      });
+      L.tileLayer(
+        "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        }
+      ).addTo(mapInstance.current);
     }
   }, []);
 
   const handleGenerateRoute = useCallback(() => {
     if (mapInstance.current) {
-      if (routeControl) {
-        mapInstance.current.removeControl(routeControl);
-      }
       generateRoute(mapInstance.current, setRouteControl);
     }
   }, [routeControl]);
 
   return (
-    <div>
-      <button title="Click" onClick={handleGenerateRoute}>Generate Route</button>
-      <div
-        ref={mapRef}
-        style={{
-          height: '500px',
-          width: '100%',
-          marginBottom: '20px',
-        }}
-      />
+    <div className="relative h-screen w-screen">
+      <button
+        id="testBtn"
+        className="absolute top-5 left-1/2 -translate-x-1/2 bg-blue-500 text-white w-32 h-12 rounded"
+        title="Click"
+        onClick={handleGenerateRoute}
+      >
+        Generate Route
+      </button>
+      <div className="absolute inset-0" id="mapElement" ref={mapRef} />
     </div>
   );
 };
