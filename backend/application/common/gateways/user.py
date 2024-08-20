92e699759d8775a26ebc05072c7a3bb782b6e414
Andrii <andryxakulishow@gmail.com>
2024-08-20 14:25:13 +0300

    Refactoring

--- backend/application/common/gateways/user.py
+++ backend/application/common/gateways/user.py
@@ -8,9 +8,11 @@ from domain.value_objects.user_id import UserId
 
 class UserReader(Protocol):
     @abstractmethod
-    async def by_id(self, user_id: UserId) -> User | None: ...
+    async def by_id(self, user_id: UserId) -> User | None:
+        raise NotImplementedError
 
 
 class UserSaver(Protocol):
     @abstractmethod
-    async def save(self, user: User) -> UserDTO: ...
+    async def save(self, user: User) -> UserDTO:
+        raise NotImplementedError
