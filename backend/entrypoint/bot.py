b73d9d287bb3a1818ae29e6df1868b150ebecef4
Andrii <andryxakulishow@gmail.com>
2024-08-29 16:05:31 +0300

    Setup webhook bot

--- backend/entrypoint/bot.py
+++ backend/entrypoint/bot.py
@@ -1,10 +1,16 @@
-import asyncio
 import logging
 
 from aiogram import Dispatcher, Bot
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
+from aiogram.exceptions import TelegramUnauthorizedError
+from aiogram.webhook.aiohttp_server import (
+    SimpleRequestHandler,
+    TokenBasedRequestHandler, setup_application
+)
 from aiogram_dialog import setup_dialogs
+from aiohttp import web
 from dishka.integrations.aiogram import setup_dishka
 
 from entrypoint.config import Config, load_config
@@ -12,7 +18,7 @@ from infrastructure.bootstrap.di import setup_di
 from presentation.bot.handlers.setup import setup_all
 
 
-def get_dispatcher() -> Dispatcher:
+def get_admin_dispatcher() -> Dispatcher:
     dp = Dispatcher()
 
     setup_dishka(container=setup_di(), router=dp, auto_inject=True)
@@ -24,21 +30,94 @@ def get_dispatcher() -> Dispatcher:
     return dp
 
 
-async def main():
+def get_shop_dispatcher() -> Dispatcher:
+    dp = Dispatcher()
+
+    return dp
+
+
+async def on_startup_admin_bot(
+        bot: Bot, config: Config, shop_bot_token: str
+) -> None:
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_webhook(
+            f"{config.webhook.webhook_url}{config.webhook.webhook_admin_path}"
+    )
+
+    await add_shop_bot(bot, shop_bot_token, config)
+
+
+async def add_shop_bot(bot: Bot, new_bot_token: str, config: Config):
+    new_bot = Bot(token=new_bot_token, session=bot.session)
+
+    try:
+        await new_bot.get_me()
+    except TelegramUnauthorizedError as err:
+        print(err)
+
+    await new_bot.delete_webhook(drop_pending_updates=True)
+
+    webhook_shop_path = config.webhook.webhook_shop_path.format(
+            bot_token=new_bot_token
+    )
+
+    await new_bot.set_webhook(
+            f"{config.webhook.webhook_url}"
+            f"{webhook_shop_path}"
+    )
+
+
+def main():
     logging.basicConfig(level=logging.INFO)
 
     config: Config = load_config()
+    session = AiohttpSession()
 
-    bot = Bot(
-            config.tg_bot.token,
-            default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    bot_settings = {
+        "session": session,
+        "default": DefaultBotProperties(parse_mode=ParseMode.HTML)
+    }
+
+    admin_bot = Bot(
+            token=config.tg_bot.admin_bot_token,
+            **bot_settings
     )
 
-    try:
-        await get_dispatcher().start_polling(bot)
-    finally:
-        await bot.session.close()
+    admin_dp = get_admin_dispatcher()
+    shop_dp = get_shop_dispatcher()
+
+    admin_request_handler = SimpleRequestHandler(
+            admin_dp, admin_bot
+    )
+
+    app = web.Application()
+    admin_request_handler.register(app, path=config.webhook.webhook_admin_path)
+
+    TokenBasedRequestHandler(
+            shop_dp,
+            bot_settings=bot_settings
+    ).register(app, path=config.webhook.webhook_shop_path)
+
+    admin_dp.startup.register(on_startup_admin_bot)
+
+    setup_application(
+            app,
+            admin_dp,
+            bot=admin_bot,
+            config=config,
+            shop_bot_token=config.tg_bot.shop_bot_token
+    )
+    setup_application(app, shop_dp)
+
+    web.run_app(
+            app,
+            host=config.webhook.webhook_host,
+            port=config.webhook.webhook_port
+    )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("Бот був вимкнений!")
--- backend/entrypoint/config.py
+++ backend/entrypoint/config.py
@@ -8,18 +8,49 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class TgBot:
-    token: str
+    admin_bot_token: str
+    shop_bot_token: str
 
     @staticmethod
     def from_env(env: Env):
-        token = env.str("BOT_TOKEN")
+        admin_bot_token = env.str("ADMIN_BOT_TOKEN")
+        shop_bot_token = env.str("SHOP_BOT_TOKEN")
 
-        return TgBot(token=token)
+        return TgBot(
+                admin_bot_token=admin_bot_token,
+                shop_bot_token=shop_bot_token
+        )
+
+
+@dataclass
+class TgWebhook:
+    webhook_url: str
+    webhook_admin_path: str
+    webhook_shop_path: str
+    webhook_host: str
+    webhook_port: int
+
+    @staticmethod
+    def from_env(env: Env):
+        webhook_url = env.str("WEBHOOK_URL")
+        webhook_admin_path = env.str("WEBHOOK_ADMIN_PATH")
+        webhook_shop_path = env.str("WEBHOOK_SHOP_PATH")
+        webhook_host = env.str("WEBHOOK_HOST")
+        webhook_port = env.int("WEBHOOK_PORT")
+
+        return TgWebhook(
+                webhook_url=webhook_url,
+                webhook_admin_path=webhook_admin_path,
+                webhook_shop_path=webhook_shop_path,
+                webhook_host=webhook_host,
+                webhook_port=webhook_port
+        )
 
 
 @dataclass
 class Config:
     tg_bot: TgBot
+    webhook: TgWebhook
 
 
 def load_config() -> Config:
@@ -27,7 +58,8 @@ def load_config() -> Config:
     env.read_env(".env")
 
     config = Config(
-        tg_bot=TgBot.from_env(env)
+            tg_bot=TgBot.from_env(env),
+            webhook=TgWebhook.from_env(env)
     )
 
     logger.info("Config successfully loaded")
