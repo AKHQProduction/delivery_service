b62668c53678bb3027f0f93c619a3a3cc4c10053
Andrii <andryxakulishow@gmail.com>
2024-09-11 13:42:11 +0300

    Test bot start interactor

--- backend/tests/unit/application/user/test_bot_start.py
+++ backend/tests/unit/application/user/test_bot_start.py
@@ -1,45 +1,77 @@
 import pytest
 
-from application.user.interactors.bot_start import (
-    BotStart,
-    BotStartInputData
-)
+from application.user.interactors.bot_start import BotStart, BotStartInputData
 from entities.user.models import UserId
 from tests.mocks.common.commiter import FakeCommiter
 from tests.mocks.common.identity_provider import FakeIdentityProvider
 from tests.mocks.gateways.user import FakeUserGateway
-from tests.unit.conftest import user_fullname
 
 
 @pytest.mark.application
 @pytest.mark.user
-async def test_bot_start(
+@pytest.mark.parametrize(
+        ["user_id"],
+        [
+            (1,),
+            (2,)
+        ]
+)
+async def test_bot_start_with_already_created_user(
         user_gateway: FakeUserGateway,
-        identity_provider: FakeIdentityProvider,
         commiter: FakeCommiter,
-        user_id: UserId,
-        user_fullname: str
+        identity_provider: FakeIdentityProvider,
+        user_id: UserId
 ) -> None:
-    interactor = BotStart(
+    action = BotStart(
             user_reader=user_gateway,
             user_saver=user_gateway,
-            identity_provider=identity_provider,
-            commiter=commiter
+            commiter=commiter,
+            identity_provider=identity_provider
     )
 
     input_data = BotStartInputData(
             user_id=user_id,
-            full_name=user_fullname
+            full_name="Test Test Test"
     )
 
-    output_data = await interactor(input_data)
+    output_data = await action(input_data)
 
     assert output_data
     assert isinstance(output_data, int)
-
     assert not user_gateway.saved
-    assert output_data == user_gateway.user.user_id
-    assert user_id == output_data
+    assert output_data == user_id
 
-    assert commiter.commited
 
+@pytest.mark.application
+@pytest.mark.user
+@pytest.mark.parametrize(
+        ["user_id"],
+        [
+            (3,),
+            (4,)
+        ]
+)
+async def test_bot_start_when_create_new_user(
+        user_gateway: FakeUserGateway,
+        commiter: FakeCommiter,
+        identity_provider: FakeIdentityProvider,
+        user_id: UserId
+) -> None:
+    action = BotStart(
+            user_reader=user_gateway,
+            user_saver=user_gateway,
+            commiter=commiter,
+            identity_provider=identity_provider
+    )
+
+    input_data = BotStartInputData(
+            user_id=user_id,
+            full_name="Test Test Test"
+    )
+
+    output_data = await action(input_data)
+
+    assert output_data
+    assert isinstance(output_data, int)
+    assert user_gateway.saved
+    assert output_data == user_id
