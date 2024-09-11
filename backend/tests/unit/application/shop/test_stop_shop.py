91758d02fae20a70243bf3be8591b641df4e6094
Andrii <andryxakulishow@gmail.com>
2024-09-11 20:06:54 +0300

    Test stop shop

---
+++ backend/tests/unit/application/shop/test_stop_shop.py
@@ -0,0 +1,62 @@
+import pytest
+
+from application.common.access_service import AccessService
+from application.errors.access import AccessDeniedError
+from application.shop.errors import UserNotHaveShopError
+
+from application.shop.interactors.delete_shop import DeleteShop
+from application.shop.interactors.stop_shop import StopShop
+from application.user.errors import UserIsNotExistError
+from entities.user.models import UserId
+from tests.mocks.common.commiter import FakeCommiter
+from tests.mocks.common.identity_provider import FakeIdentityProvider
+from tests.mocks.common.webhook_manager import FakeWebhookManager
+from tests.mocks.gateways.shop import FakeShopGateway
+
+
+@pytest.mark.application
+@pytest.mark.shop
+@pytest.mark.parametrize(
+        ["user_id", "shop_id", "exc_class"],
+        [
+            (1, 1234567898, None),
+            (4, 9876543212, UserIsNotExistError),
+            (3, 1234567898, AccessDeniedError),
+            (2, 1234567898, UserNotHaveShopError)
+        ]
+)
+async def test_stop_shop(
+        shop_gateway: FakeShopGateway,
+        identity_provider: FakeIdentityProvider,
+        webhook_manager: FakeWebhookManager,
+        commiter: FakeCommiter,
+        access_service: AccessService,
+        user_id: UserId,
+        shop_id: int,
+        exc_class
+) -> None:
+    action = StopShop(
+            shop_reader=shop_gateway,
+            identity_provider=identity_provider,
+            webhook_manager=webhook_manager,
+            commiter=commiter,
+            access_service=access_service,
+    )
+
+    coro = action()
+
+    if exc_class:
+        with pytest.raises(exc_class):
+            await coro
+
+        assert not commiter.commited
+        assert not webhook_manager.dropped
+
+    else:
+        output_data = await coro
+
+        assert not output_data
+
+        assert commiter.commited
+        assert webhook_manager.dropped
+        assert shop_id in shop_gateway.shops.keys()
