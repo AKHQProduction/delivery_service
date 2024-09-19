b10848e1d227f7aa42a0b0aa1aaf90d09bf2990c
Andrii <andryxakulishow@gmail.com>
2024-09-19 15:33:10 +0300

    Refactoring

--- backend/presentation/admin/handlers/admin/staff/main/dialog.py
+++ backend/presentation/admin/handlers/admin/staff/main/dialog.py
@@ -10,7 +10,7 @@ from dishka import FromDishka
 from dishka.integrations.aiogram_dialog import inject
 from magic_filter import MagicFilter
 
-from application.common.request_data import Pagination
+from application.common.input_data import Pagination
 from application.user.gateway import GetUsersFilters
 from application.user.interactors.get_users import GetUsers, GetUsersInputData
 from entities.user.models import UserId
