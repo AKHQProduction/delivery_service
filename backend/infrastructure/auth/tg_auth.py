672f33cb951cee3875afa3d3bca4ea3f33b9be3c
Andrii <andryxakulishow@gmail.com>
2024-09-29 16:19:58 +0300

    Role from id provider

--- backend/infrastructure/auth/tg_auth.py
+++ backend/infrastructure/auth/tg_auth.py
@@ -1,5 +1,7 @@
 from application.common.identity_provider import IdentityProvider
+from application.employee.gateway import EmployeeReader
 from application.user.gateway import UserReader
+from entities.employee.models import EmployeeRole
 from entities.user.models import User, UserId
 
 
@@ -8,9 +10,18 @@ class TgIdentityProvider(IdentityProvider):
         self,
         user_id: int,
         user_gateway: UserReader,
+        employee_gateway: EmployeeReader,
     ):
         self._user_id = user_id
         self._user_gateway = user_gateway
+        self._employee_gateway = employee_gateway
 
     async def get_user(self) -> User:
         return await self._user_gateway.by_id(UserId(self._user_id))
+
+    async def get_role(self) -> EmployeeRole:
+        employee = await self._employee_gateway.by_identity(
+            UserId(self._user_id)
+        )
+
+        return employee.role
