9eb657f8c28a866725ff81e129a914d916ebcc2f
PlzTrustMe <andryxakulishow@gmail.com>
2025-11-11 03:49:08 +0200

    Refactor: provide handlers for core logic

--- backend/src/backend/presentation/admin_bot/dialogs/new_shop.py
+++ backend/src/backend/presentation/admin_bot/dialogs/new_shop.py
@@ -4,7 +4,13 @@ from aiogram.types import Message, User
 from aiogram_dialog import Dialog, DialogManager, Window
 from aiogram_dialog.widgets.input import TextInput
 from aiogram_dialog.widgets.text import Const
+from dishka import FromDishka
+from dishka.integrations.aiogram_dialog import inject
 
+from backend.application.commands import (
+    CreateNewShopCommand,
+    CreateNewShopCommandHandler,
+)
 from backend.presentation.admin_bot import states
 from backend.presentation.admin_bot.keyboards.inline import shop_kb
 
@@ -13,11 +19,18 @@ if TYPE_CHECKING:
     from aiogram.types import User
 
 
+@inject
 async def on_input_shp_name(
-    _: Message, __: Any, manager: DialogManager, ___: str
+    _: Message,
+    __: Any,
+    manager: DialogManager,
+    value: str,
+    handler: FromDishka[CreateNewShopCommandHandler],
 ) -> None:
     bot: Bot = cast("Bot", manager.middleware_data.get("bot"))
     user: User = cast("User", manager.middleware_data.get("event_from_user"))
+
+    await handler.handle(CreateNewShopCommand(name=value))
 
     await bot.send_message(
         chat_id=user.id,
--- backend/src/backend/presentation/admin_bot/handlers/user/start.py
+++ backend/src/backend/presentation/admin_bot/handlers/user/start.py
@@ -5,7 +5,12 @@ from aiogram.enums import ChatType
 from aiogram.filters import CommandStart
 from aiogram.types import Message, User
 from aiogram_dialog import DialogManager, StartMode
+from dishka import FromDishka
 
+from backend.application.commands import (
+    BotStartCommand,
+    BotStartCommandHandler,
+)
 from backend.presentation.admin_bot import states
 from backend.presentation.admin_bot.keyboards.inline import shop_kb
 
@@ -14,11 +19,15 @@ router = Router()
 
 @router.message(CommandStart(), F.chat.type == ChatType.PRIVATE)
 async def cmd_start(
-    message: Message, dialog_manager: DialogManager
+    message: Message,
+    dialog_manager: DialogManager,
+    handler: FromDishka[BotStartCommandHandler],
 ) -> Message | None:
     user: User = cast("User", message.from_user)
 
-    exists = True
+    exists = await handler.handle(
+        BotStartCommand(tg_id=user.id, full_name=user.full_name)
+    )
     if exists:
         return await message.answer(
             f"🙋 Привіт, {user.first_name}!", reply_markup=shop_kb()
